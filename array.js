e0b61e41fe1a76ab1583315ea41471757c3f10ad
Any gifters ??? <57357698+Ian808@users.noreply.github.com>
2020-04-14 14:29:56 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,13 @@
+var array1 = [3, 5, 9];
+var array2 = [1, 7, 2];
+var maal = 2;
+var taettest = 1000;
+var svar = ([3, 5]);
+
+for (var i = 0; i < array1.length; i++) {
+    for (var j = 0; j < array2.length; j++) {
+        console.log(array1[i] + " " + array2[j]);
+    }
+}
+
+
